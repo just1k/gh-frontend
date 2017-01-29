615e0f925b9059c67f36337a4cae397a2ef5db7c
just1k <sergey.zborovskiy@gmail.com>
2017-01-29 07:53:05 +0200

    Facebook, get-in-touch, footer html+scss

---
+++ lesson14/free-agency/app/js/facebook-form.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function () {
+   
+
+   $("#facebook-analysis").on("click","span.fa", function (event) {
+        
+        $("form.facebook-form").toggle();
+
+    });
+
+
+
+
+
+ 
+
+
+
+
+
+
+});
