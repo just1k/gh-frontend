0a56c8b67bcdfc5f6c04fd50898c821eb0dd6b23
just1k <sergey.zborovskiy@gmail.com>
2017-02-05 12:56:49 +0200

    Finished styled all. Beta lesson15 done

--- lesson15/app/js/common.js
+++ lesson15/app/js/common.js
@@ -15,3 +15,25 @@ $(document).ready(function () {
 
             });
         });
+
+
+$(document).ready(function(){
+		$("#btn-up").on("click","a", function (event) {
+		    event.preventDefault();
+		    var id  = $(this).attr('href'),
+		        top = $(id).offset().top;
+		    $('body,html').animate({scrollTop: top}, 1500);
+		});
+	});
+
+
+
+$(document).ready(function () {
+   
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
